39926ec0801917cfd1dd11851a4da57332564876
Albert Krewinkel <albert@zeitkraut.de>
2022-08-09 14:16:18 +0200

    Release  0.0.1

--- _extensions/test-extension/test-extension.lua
+++ _extensions/test-extension/test-extension.lua
@@ -1,1 +1,8 @@
-../../test-extension.lua
+function Meta (meta)
+  local title
+
+  meta.title = {'Test: '} .. pandoc.utils.blocks_to_inlines(
+    pandoc.Blocks{meta.title}
+  )
+  return meta
+end
--- test-extension.lua
+++ test-extension.lua
@@ -1,4 +1,6 @@
 function Meta (meta)
+  local title
+
   meta.title = {'Test: '} .. pandoc.utils.blocks_to_inlines(
     pandoc.Blocks{meta.title}
   )
